3bcc77c5dd2db7962a838373427b761df9c826ad
tenistu <sonicx666@hotmail.fr>
2012-11-21 17:18:45 +0100

    Premier script en python avec les commentaires

---
+++ base-python.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+####################################
+##			          ##
+##  LICENCE GPL V3                ##
+####################################
+
+# CONTRIBUTEUR :
+# - TENISTU
+
+# BASE DE PYTHON
+# 1 - SYNTAXE DES BOUCLES ET DES TESTES À TRAVERS L'ÉCRITURE
+
+
+print("pile")
